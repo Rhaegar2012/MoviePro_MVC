09b1b5722b978730550b459973ab5eeec86ba3ea
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-11-15 15:19:56 -0800

    11152021-Updated
    Migrated project for Visual studio 2022
    -Succesfully migrated data and launched the skeleton website

---
+++ MoviePro_MVC/MoviePro_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviePro_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
