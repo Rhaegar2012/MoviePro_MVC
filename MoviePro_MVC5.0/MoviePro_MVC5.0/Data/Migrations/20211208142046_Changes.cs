37b8befa36357dbd902d7a5c49cf8ad857a7bba2
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-12-08 06:59:11 -0800

    12082021-Update
    Added landing page view and solved various issues with the carousel display and title cards
    
    Issues:
    Several cards are out of alignment , HTML needs to be reviewed in thorough detail
    
    The  movie details controller is broken , it appears to have unresolved references to TMDB database service

---
+++ MoviePro_MVC5.0/MoviePro_MVC5.0/Data/Migrations/20211208142046_Changes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviePro_MVC5._0.Data.Migrations
+{
+    public partial class Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "MovieCollection",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "RuntTime",
+                table: "Movie",
+                newName: "RunTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "MovieCollection",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "RunTime",
+                table: "Movie",
+                newName: "RuntTime");
+        }
+    }
+}
